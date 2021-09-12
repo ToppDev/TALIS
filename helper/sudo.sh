694750ac2cd0c2046910035145ea7e7a66bd3b02
Thomas Topp <thomas@topp.cc>
2021-09-12 23:17:00 +0200

    Sudo temp permissions

--- helper/sudo.sh
+++ helper/sudo.sh
@@ -13,9 +13,15 @@ sudoperms() {
             echo "$perm #TALIS" >> /etc/sudoers
         done
     else
+        # Temporary entry to not loose sudo right in the process
+        sudo sh -c "echo '%wheel ALL=(ALL) NOPASSWD: ALL #TMP' >> /etc/sudoers"
+
         sudo sed -i "/#TALIS/d" /etc/sudoers
         for perm in "$@"; do
             sudo sh -c "echo '$perm #TALIS' >> /etc/sudoers"
         done
+
+        # Remove Temporary sudo rights
+        sudo sed -i "/#TMP/d" /etc/sudoers
     fi
 }
