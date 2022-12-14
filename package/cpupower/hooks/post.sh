39c5423af812088776ac75c7394993caa1202eb0
Thomas Topp <thomas@topp.cc>
2022-12-14 08:41:09 +0100

    Updated progs

---
+++ package/cpupower/hooks/post.sh
@@ -0,0 +1,22 @@
+#!/bin/sh
+# ToppDev's Artix/Arch Linux Installation Scripts (TALIS)
+# by Thomas Topp <dev@topp.cc>
+# License: GNU GPLv3
+
+# ########################################################################################################## #
+#                                               Helper scripts                                               #
+# ########################################################################################################## #
+
+packagedir="$( cd "$(dirname "$0")" >/dev/null 2>&1 ; pwd -P )/.."
+
+source "$scriptdir/helper/color.sh"
+source "$scriptdir/helper/log.sh"
+source "$scriptdir/helper/install.sh"
+source "$scriptdir/helper/checkArchRootInternet.sh"
+
+# ########################################################################################################## #
+#                                                   Script                                                   #
+# ########################################################################################################## #
+
+sudo systemctl start cpupower.service
+sudo systemctl enable cpupower.service
