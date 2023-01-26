f4c8293c1727a7af9eccf28c246874a94f25cc65
Thomas Topp <thomas@topp.cc>
2023-01-26 06:05:43 +0100

    Rust install

---
+++ package/rustup/hooks/post.sh
@@ -0,0 +1,26 @@
+#!/bin/sh
+# ToppDev's Artix/Arch Linux Installation Scripts (TALIS)
+# by Thomas Topp <dev@topp.cc>
+# License: GNU GPLv3
+
+# ########################################################################################################## #
+#                                               Helper scripts                                               #
+# ########################################################################################################## #
+
+packagedir="$( cd "$(dirname "$0")" >/dev/null 2>&1 ; pwd -P )/.."
+
+source "$scriptdir/helper/color.sh"
+source "$scriptdir/helper/log.sh"
+source "$scriptdir/helper/install.sh"
+source "$scriptdir/helper/checkArchRootInternet.sh"
+
+# ########################################################################################################## #
+#                                                   Script                                                   #
+# ########################################################################################################## #
+
+# Install the toolchain
+rustup default stable
+
+rustup component add rust-src
+rustup component add clippy
+rustup component add rustfmt
