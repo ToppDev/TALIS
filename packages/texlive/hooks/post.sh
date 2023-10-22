ad9e670d433a26445eb341f09c9b88b5b841b2d7
Thomas Topp <thomas@topp.cc>
2023-10-22 13:03:06 +0200

    Texlive hook

---
+++ packages/texlive/hooks/post.sh
@@ -0,0 +1,21 @@
+#!/bin/sh
+# ToppDev's Artix/Arch Linux Installation Scripts (TALIS)
+# by Thomas Topp <dev@topp.cc>
+# License: GNU GPLv3
+
+# ########################################################################################################## #
+#                                               Helper scripts                                               #
+# ########################################################################################################## #
+
+packagedir="$( cd "$(dirname "$0")" >/dev/null 2>&1 ; pwd -P )/.."
+
+source "$scriptdir/helper/color.sh"
+source "$scriptdir/helper/log.sh"
+source "$scriptdir/helper/install.sh"
+source "$scriptdir/helper/checkArchRootInternet.sh"
+
+# ########################################################################################################## #
+#                                                   Script                                                   #
+# ########################################################################################################## #
+
+installaur perl-log-log4perl perl-file-homedir perl-log-dispatch perl-namespace-autoclean perl-specio perl-eval-closure perl-params-validationcompiler perl-yaml-tiny perl-unicode-linebreak
